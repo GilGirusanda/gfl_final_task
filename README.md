de9ff1f4f836780984a2d013a2d458b88fa65024
GilGirusanda <egor.babinin@gmail.com>
2024-03-24 18:10:32 +0200

    Small feats

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 "# gfl_final_task" 
+## How to launch
+### Assign values for environment variables:
+1. DB_PASSWORD 
+2. DB_USERNAME
+3. PGSQL_HOST
+
+Use intellij idea or<br>
+Execute `mvnw clean install` and then `mvnw spring-boot:run` commands in console to run the app.
--- src/main/java/org/project/fin/Icpo2Application.java
+++ src/main/java/org/project/fin/Icpo2Application.java
@@ -1,5 +1,8 @@
 package org.project.fin;
 
+import org.project.fin.models.Language;
+import org.project.fin.services.CriminalService;
+import org.project.fin.services.LanguageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,23 +17,20 @@ public class Icpo2Application {
         SpringApplication.run(Icpo2Application.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner cmd(CriminalService criminalService){
-//        return args -> {
-//
-//            Criminal criminal = Criminal.builder(
-//                    "John",
-//                    new SpecialSigns(1.8f),
-//                    CriminalProfession.BURGLAR,
-//                    "A man stole a blanket from the shop",
-//                    LocalDate.of(1997, 7, 1),
-//                    Citizenship.Canada).build();
-//
-//            boolean isSuccess = criminalService.save(criminal);
-//            System.out.println("Saved: " + isSuccess);
-//
-//            criminalService.findAll().forEach(System.out::println);
-//        };
-//    }
+    @Bean
+    public CommandLineRunner cmd(LanguageService languageService){
+        return args -> {
+            // Fill `languages` table with test values
+            languageService.save(new Language(null, "Ukrainian", null));
+            languageService.save(new Language(null, "English(UK)", null));
+            languageService.save(new Language(null, "English(USA)", null));
+            languageService.save(new Language(null, "French", null));
+            languageService.save(new Language(null, "German", null));
+            languageService.save(new Language(null, "Italian", null));
+            languageService.save(new Language(null, "Portuguese", null));
+            languageService.save(new Language(null, "Sweden", null));
+            languageService.save(new Language(null, "Canadian", null));
+        };
+    }
 
 }
